f9211c5ccdd073d55a1abba932d1d3b87264bb8f
Rolo27s <108833691+Rolo27s@users.noreply.github.com>
2023-01-22 12:30:21 +0100

    Add files via upload

---
+++ 1. Bases de JavaScript/EJ_3_operacionesBasicas.js
@@ -0,0 +1,8 @@
+let numero1 = 33;
+let numero2 = 67;
+
+console.log (numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+console.log (numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+console.log (numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+console.log (numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+console.log (numero1 + " % " + numero2 + " = " + (numero1 % numero2));
